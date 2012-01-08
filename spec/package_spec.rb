9c37b722c047f6556d4dc662ba2a7b7726547859
Eric J. Holmes <eric@ejholmes.net>
2012-01-08 15:51:22 -0800

    Add xml fixture

--- spec/package_spec.rb
+++ spec/package_spec.rb
@@ -1,38 +1,25 @@
 require "metaforce/package"
 require "nokogiri"
 
-test_xml = <<-XML
-<?xml version="1.0"?>
-<Package xmlns="http://soap.sforce.com/2006/04/metadata">
-  <types>
-    <members>TestClass</members>
-    <name>ApexClass</name>
-  </types>
-  <types>
-    <members>Component</members>
-    <name>ApexComponent</name>
-  </types>
-  <version>23.0</version>
-</Package>
-XML
-
 test_hash = {
   :apex_class => ['TestClass'],
   :apex_component => ['Component']
 }
 
 describe Metaforce::Package do
+  package_xml = File.open(File.join(File.dirname(__FILE__), 'fixtures/package.xml'), 'r').read
+
   context "When given a hash" do
     it "returns a string containing xml" do
       package = Metaforce::Package.new(test_hash)
       response = package.to_xml
-      response.should eq(test_xml)
+      response.should eq(package_xml)
     end
   end
   context "Parse" do
     it "returns a hash" do
       package = Metaforce::Package.new
-      response = package.parse(test_xml).to_hash
+      response = package.parse(package_xml).to_hash
       response.should eq(test_hash)
     end
   end
