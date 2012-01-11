ffa43771369f3082bff97bb9b0144960641e0c6e
Eric J. Holmes <eric@ejholmes.net>
2012-01-10 22:25:02 -0800

    Get the metadata server url as well

--- lib/metaforce/api/services.rb
+++ lib/metaforce/api/services.rb
@@ -2,9 +2,13 @@ require 'savon'
 
 module Metaforce
   module Services
-    attr_reader :session_id
-
     class Client
+      attr_reader :session
+
+      def initialize(options={})
+        @session = self.login(options[:username], options[:password], options[:security_token])
+      end
+
       def login(username, password, security_token=nil)
         password = "#{password}#{security_token}" unless security_token.nil?
         client = Savon::Client.new File.expand_path("../../../../wsdl/23.0/partner.xml", __FILE__)
@@ -14,7 +18,8 @@ module Metaforce
             :password => password
           }
         end
-        @session_id = response.body[:login_response][:result][:session_id]
+        { :session_id => response.body[:login_response][:result][:session_id],
+          :metadata_server_url =>  response.body[:login_response][:result][:metadata_server_url] }
       end
     end
   end
--- spec/lib/api/services_spec.rb
+++ spec/lib/api/services_spec.rb
@@ -4,14 +4,14 @@ require "metaforce"
 describe Metaforce::Services::Client do
   context "login" do
     it "raises an error with invalid credentials" do
-      services = Metaforce::Services::Client.new
       savon.expects(:login).with(:username => 'invalid', :password => 'password').returns(:failure)
-      expect { services.login('invalid', 'password') }.to raise_error
+      expect { Metaforce::Services::Client.new(:username => 'invalid', :password => 'password') }.to raise_error
     end
-    it "logs a user in" do
-      services = Metaforce::Services::Client.new
+    it "logs a user in and gets the session" do
       savon.expects(:login).with(:username => 'valid', :password => 'password').returns(:success)
-      services.login('valid', 'password').should eq('00DU0000000Ilbh!AQoAQHVcube9Z6CRlbR9Eg8ZxpJlrJ6X8QDbnokfyVZItFKzJsLHIRGiqhzJkYsNYRkd3UVA9.s82sbjEbZGUqP3mG6TP_P8')
+      session = Metaforce::Services::Client.new(:username => 'valid', :password => 'password').session
+      session.should eq({ :session_id => "00DU0000000Ilbh!AQoAQHVcube9Z6CRlbR9Eg8ZxpJlrJ6X8QDbnokfyVZItFKzJsLHIRGiqhzJkYsNYRkd3UVA9.s82sbjEbZGUqP3mG6TP_P8",
+                          :metadata_server_url => "https://na12-api.salesforce.com/services/Soap/m/23.0/00DU0000000Albh" })
     end
   end
 end
