baec94bfd3d5b05626c9280ac95d7c159f5bd061
Eric J. Holmes <eric@ejholmes.net>
2012-01-10 23:38:40 -0800

    Dear Soap...I hate you

--- lib/metaforce/api/metadata.rb
+++ lib/metaforce/api/metadata.rb
@@ -3,8 +3,13 @@ require 'savon'
 module Metaforce
   module Metadata
     class Client
-      def initialize
-        # @session_id = Services.new.login()
+      def initialize(options={})
+        @session = Services::Client.new(options).session
+      end
+
+      def list
+        client = Savon::Client.new File.expand_path("../../../../wsdl/23.0/metadata.xml", __FILE__)
+        response = client.request(:list_metadata)
       end
     end
   end
--- spec/lib/api/metadata_spec.rb
+++ spec/lib/api/metadata_spec.rb
@@ -2,4 +2,11 @@ require File.expand_path('../../../spec_helper', __FILE__)
 require "metaforce/api/metadata"
 
 describe Metaforce::Metadata::Client do
+  context "list metadata" do
+    it "does something" do
+      savon.expects(:login).with(:username => 'valid', :password => 'password').returns(:success)
+      client = Metaforce::Metadata::Client.new(:username => 'valid', :password => 'password')
+      puts client.list
+    end
+  end
 end
