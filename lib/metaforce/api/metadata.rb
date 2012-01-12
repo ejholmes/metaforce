fdc039d9869852216c5fd21af275cec94dafc1ac
Eric J. Holmes <eric@ejholmes.net>
2012-01-11 22:00:07 -0800

    Basic tests for list and describe

--- lib/metaforce/api/metadata.rb
+++ lib/metaforce/api/metadata.rb
@@ -8,6 +8,11 @@ module Metaforce
         @client = Savon::Client.new File.expand_path("../../../../wsdl/23.0/metadata.xml", __FILE__) do |wsdl|
           wsdl.endpoint = @session[:metadata_server_url]
         end
+        @header = {
+            "ins0:SessionHeader" => {
+              "ins0:sessionId" => @session[:session_id]
+            }
+        }
       end
 
       # Specify an array of component types to list
@@ -22,16 +27,20 @@ module Metaforce
           queries = [ queries ]
         end
         response = @client.request(:list_metadata) do |soap|
-          soap.header = {
-            "ins0:SessionHeader" => {
-              "ins0:sessionId" => @session[:session_id]
-            }
-          }
+          soap.header = @header
           soap.body = {
             :queries => queries
           }
         end
-        response.body
+        response.body[:list_metadata_response][:result]
+      end
+
+      # Describe the organization's metadata
+      def describe
+        response = @client.request(:describe_metadata) do |soap|
+          soap.header = @header
+        end
+        response.body[:describe_metadata_response][:result]
       end
     end
   end
--- spec/lib/api/metadata_spec.rb
+++ spec/lib/api/metadata_spec.rb
@@ -3,10 +3,19 @@ require "metaforce/api/metadata"
 
 describe Metaforce::Metadata::Client do
   context "list metadata" do
-    it "does something" do
-      # savon.expects(:login).with(:username => 'valid', :password => 'password').returns(:success)
-      # client = Metaforce::Metadata::Client.new(:username => 'valid', :password => 'password')
-      # puts client.list
+    it "lists metadata" do
+      savon.expects(:login).with(:username => 'valid', :password => 'password').returns(:success)
+      client = Metaforce::Metadata::Client.new(:username => 'valid', :password => 'password')
+      savon.expects(:list_metadata).with(:queries => [ :type => "ApexClass"]).returns(:objects)
+      client.list(:type => "ApexClass").class.should eq(Array)
+    end
+  end
+  context "describe metadata" do
+    it "describes the organization" do
+      savon.expects(:login).with(:username => 'valid', :password => 'password').returns(:success)
+      client = Metaforce::Metadata::Client.new(:username => 'valid', :password => 'password')
+      savon.expects(:describe_metadata).returns(:success)
+      client.describe.class.should eq(Hash)
     end
   end
 end
