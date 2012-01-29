c6b47e77ad40471130c9ff24591a1d767d42e4ba
Eric J. Holmes <eric@ejholmes.net>
2012-01-29 12:52:23 -0800

    Add rdoc

--- lib/metaforce/config.rb
+++ lib/metaforce/config.rb
@@ -1,19 +1,35 @@
 module Metaforce
   class << self
+    # Returns the current Configuration
+    #
+    #    Metaforce.configuration.username = "username"
+    #    Metaforce.configuration.password = "password"
     def configuration
       @configuration ||= Configuration.new
     end
 
+    # Yields the Configuration
+    #
+    #    Metaforce.configure do |config|
+    #      config.username = "username"
+    #      config.password = "password"
+    #    end
     def configure
       yield configuration
     end
   end
 
   class Configuration
+    # The Salesforce API version to use. Defaults to 23.0
     attr_accessor :api_version
+    # The username to use during login.
     attr_accessor :username
+    # The password to use during login.
     attr_accessor :password
+    # The security token to use during login.
     attr_accessor :security_token
+    # Set this to true if you're authentication with a Sandbox instance.
+    # Defaults to false.
     attr_accessor :test
 
     def initialize
