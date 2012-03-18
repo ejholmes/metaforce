2733fa03bc28bf9b8e0e8652a061370cf9d54924
Eric Holmes <ejholmes@eric-macbook.isengard.ejholmes.net>
2012-03-18 11:36:13 -0700

    Add .underscore method.

--- spec/lib/core_extensions/string_spec.rb
+++ spec/lib/core_extensions/string_spec.rb
@@ -2,8 +2,20 @@ require "spec_helper"
 
 describe String do
 
-  it "camelCases underscored_words" do
-    "hello_world".camelCase.should eq("helloWorld")
+  describe ".camelCase" do
+
+    it "camelCases underscored_words" do
+      "hello_world".camelCase.should eq("helloWorld")
+    end
+
+  end
+
+  describe ".underscore" do
+
+    it "underscores CamelCased words" do
+      "HelloWorld".underscore.should eq("hello_world")
+    end
+
   end
 
 end
