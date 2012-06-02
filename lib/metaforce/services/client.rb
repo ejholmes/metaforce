1ced6005df33e72630a2d9d1a90ad39c7776e2df
Eric J. Holmes <eric@ejholmes.net>
2012-06-01 18:50:36 -0700

    Add method for .describe_layout.

--- lib/metaforce/services/client.rb
+++ lib/metaforce/services/client.rb
@@ -20,25 +20,48 @@ module Metaforce
           :security_token => Metaforce.configuration.security_token
         } if options.nil?
         @session = self.login(options[:username], options[:password], options[:security_token])
+
+        @client = Savon::Client.new File.expand_path("../../../../wsdl/#{Metaforce.configuration.api_version}/partner.xml", __FILE__) do |wsdl|
+          wsdl.endpoint = @session[:services_url]
+        end
+        @client.http.auth.ssl.verify_mode = :none
+        @header = {
+            "ins0:SessionHeader" => {
+              "ins0:sessionId" => @session[:session_id]
+            }
+        }
       end
 
       # Performs a login and sets @session
       def login(username, password, security_token=nil)
         password = "#{password}#{security_token}" unless security_token.nil?
-        client = Savon::Client.new File.expand_path("../../../../wsdl/#{Metaforce.configuration.api_version}/partner.xml", __FILE__) do |wsdl|
+        @client = Savon::Client.new File.expand_path("../../../../wsdl/#{Metaforce.configuration.api_version}/partner.xml", __FILE__) do |wsdl|
           wsdl.endpoint = wsdl.endpoint.to_s.sub(/login/, 'test') if Metaforce.configuration.test
           Metaforce.log("Logging in via #{wsdl.endpoint.to_s}")
         end
-        client.http.auth.ssl.verify_mode = :none
+        @client.http.auth.ssl.verify_mode = :none
 
-        response = client.request(:login) do
+        response = @client.request(:login) do
           soap.body = {
             :username => username,
             :password => password
           }
         end
         { :session_id => response.body[:login_response][:result][:session_id],
-          :metadata_server_url =>  response.body[:login_response][:result][:metadata_server_url] }
+          :metadata_server_url =>  response.body[:login_response][:result][:metadata_server_url],
+          :services_url => response.body[:login_response][:result][:server_url] }
+      end
+
+      def describe_layout(sobject, record_type_id=nil)
+        body = {
+          'sObjectType' => sobject
+        }
+        body['recordTypeID'] = record_type_id if record_type_id
+        response = @client.request(:describe_layout) do |soap|
+          soap.header = @header
+          soap.body = body
+        end
+        response.body[:describe_layout_response][:result]
       end
     end
   end
