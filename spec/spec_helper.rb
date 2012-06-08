0534a75bdc647bb33909b1e8fadf1e3153e8903b
Eric J. Holmes <eric@ejholmes.net>
2012-06-08 14:04:55 -0700

    Rauthentication for services client.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,5 +7,7 @@ RSpec.configure do |config|
   config.include Savon::Spec::Macros
 end
 
-Savon.log = false
+Savon.configure do |config|
+  config.log = false
+end
 Savon::Spec::Fixture.path = File.join(File.dirname(__FILE__), 'fixtures/requests')
