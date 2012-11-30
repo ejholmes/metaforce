67b30c8a8b93fd1f2df60f554ee1154dba969b9e
Eric J. Holmes <eric@ejholmes.net>
2012-11-30 10:19:21 -0800

    Cleanup.

--- lib/metaforce/services/client.rb
+++ lib/metaforce/services/client.rb
@@ -13,13 +13,13 @@ module Metaforce
         }
         body['recordTypeID'] = record_type_id if record_type_id
         response = request(:describe_layout) do |soap|
-          soap.header = soap_headers
           soap.body = body
         end
         Hashie::Mash.new(response.body).describe_layout_response.result
       end
 
       # Get active picklists for a record type.
+      # TODO: Some serious smell here, abstract this into a Layout class?
       def picklist_values(sobject, record_type_id, field)
         picklists = describe_layout(sobject, record_type_id).record_type_mappings.picklists_for_record_type
         picklists.select { |p| p.picklist_name == field }.first.picklist_values
