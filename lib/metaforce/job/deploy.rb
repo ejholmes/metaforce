1e08ae6185235d829c83d5fa0e2a23153f29cbe4
Eric J. Holmes <eric@ejholmes.net>
2012-11-30 09:37:49 -0800

    Deploy job spec.

--- lib/metaforce/job/deploy.rb
+++ lib/metaforce/job/deploy.rb
@@ -2,13 +2,18 @@ module Metaforce
   class Job::Deploy < Job
     attr_reader :id
 
-    def initialize(client, path, options)
+    def initialize(client, path, options={})
       super(client)
       @path, @options = path, options
     end
 
     def perform
-      @id = client.deploy(zip_file, @options)
+      @id = client.deploy(payload, @options)
+    end
+
+    # Public: Base64 encodes the contents of the zip file.
+    def payload
+      Base64.encode64(File.open(@path, 'rb').read)
     end
   end
 end
--- spec/lib/job/deploy_spec.rb
+++ spec/lib/job/deploy_spec.rb
@@ -1,5 +1,20 @@
 require 'spec_helper'
 
 describe Metaforce::Job::Deploy do
-  let(:job) { described_class.new }
+  let(:client) { double('metadata client') }
+  let(:job) { described_class.new client, File.expand_path('../../../fixtures/payload.zip', __FILE__) }
+
+  describe '.payload' do
+    subject { job.payload }
+    it { should be_a String }
+  end
+
+  describe '.perform' do
+    before do
+      client.should_receive(:deploy).with(job.payload, {}).and_return('1234')
+    end
+
+    subject { job.perform }
+    it { should be_a String }
+  end
 end
