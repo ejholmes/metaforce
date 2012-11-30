c4a767bb3a6cb47b02803c5b5fe0c16264604eaf
Eric J. Holmes <eric@ejholmes.net>
2012-11-30 10:21:09 -0800

    Cleanup.

--- lib/metaforce/services/client.rb
+++ lib/metaforce/services/client.rb
@@ -21,8 +21,8 @@ module Metaforce
       # Get active picklists for a record type.
       # TODO: Some serious smell here, abstract this into a Layout class?
       def picklist_values(sobject, record_type_id, field)
-        picklists = describe_layout(sobject, record_type_id).record_type_mappings.picklists_for_record_type
-        picklists.select { |p| p.picklist_name == field }.first.picklist_values
+        describe_layout(sobject, record_type_id).record_type_mappings.picklists_for_record_type
+          .select { |p| p.picklist_name == field }.first.picklist_values
           .select { |p| p.active }.collect { |p| [ p.label, p.value ] }
       end
     end
