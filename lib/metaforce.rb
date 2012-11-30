f38492e067f9f935ce80fca28f376c4dac02b70f
Eric J. Holmes <eric@ejholmes.net>
2012-11-30 00:18:21 -0800

    Client registry.

--- lib/metaforce.rb
+++ lib/metaforce.rb
@@ -11,6 +11,22 @@ require 'metaforce/metadata'
 
 module Metaforce
   class << self
+    def new(options)
+      Class.new do
+        def initialize(options)
+          @options = options
+        end
+
+        def metadata
+          @metadata ||= Metadata::Client.new(@options)
+        end
+
+        def services
+          @services ||= Services::Client.new(@options)
+        end
+      end.new(options)
+    end
+
     # Performs a login and retrurns the session
     def login(username, password, security_token=nil)
       Login.new(username, password, security_token).login
