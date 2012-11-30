17dcdaf3d574ee10672992e11f762ea2cb48135d
Eric J. Holmes <eric@ejholmes.net>
2012-11-30 15:56:43 -0800

    Cleanup

--- lib/metaforce/job.rb
+++ lib/metaforce/job.rb
@@ -1,5 +1,9 @@
 module Metaforce
   class Job
+    autoload :Deploy,   'metaforce/job/deploy'
+    autoload :Retrieve, 'metaforce/job/retrieve'
+
+    # Job id.
     attr_reader :id
 
     class << self
@@ -14,26 +18,27 @@ module Metaforce
       @client = client
     end
 
+    # Public: Perform the job.
     def perform
       raise 'not implemented.'
     end
 
+    # Public: Queries the job status from the API.
     def status
       client.status(id, status_type)
     end
 
+    # Public: Returns true if the job has completed. 
     def done?
       status.done
     end
 
   private
 
+    # Internal: The Metaforce::Metadata::Client instance.
     def client
       @client
     end
 
   end
 end
-
-require 'metaforce/job/deploy'
-require 'metaforce/job/retrieve'
