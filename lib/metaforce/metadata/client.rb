65bf679691c57135691e395d0dd11ae98501fdcb
Eric J. Holmes <eric@ejholmes.net>
2012-11-30 18:03:31 -0800

    Comments.

--- lib/metaforce/metadata/client.rb
+++ lib/metaforce/metadata/client.rb
@@ -4,7 +4,7 @@ module Metaforce
       endpoint :metadata_server_url
       wsdl Metaforce.configuration.metadata_wsdl
 
-      # Specify an array of component types to list.
+      # Public: Specify an array of component types to list.
       #
       # Examples
       #
@@ -23,6 +23,15 @@ module Metaforce
         Hashie::Mash.new(response.body).list_metadata_response.result
       end
 
+      # Public: Describe the organization's metadata.
+      #
+      # version - API version (default: latest).
+      #
+      # Examples
+      #
+      #   # List the names of all metadata types
+      #   client.describe.metadata_objects.collect { |t| t.xml_name }
+      #   #=> ["CustomLabels", "StaticResource", "Scontrol", "ApexComponent", ... ]
       def describe(version=nil)
         response = request(:describe_metadata) do |soap|
           soap.body = { :api_version => version } unless version.nil?
@@ -30,6 +39,15 @@ module Metaforce
         Hashie::Mash.new(response.body).describe_metadata_response.result
       end
 
+      # Public: Checks the status of an async result.
+      #
+      # ids  - A list of ids to check.
+      # type - either :deploy or :retrieve
+      #
+      # Examples
+      # 
+      #   client.status('04sU0000000Wx6KIAS')
+      #   #=> {:done=>true, :id=>"04sU0000000Wx6KIAS", :state=>"Completed", ...}
       def status(ids, type=nil)
         method = :check_status
         method = :"check_#{type}_status" if type
@@ -40,6 +58,12 @@ module Metaforce
         Hashie::Mash.new(response.body)[:"#{method}_response"].result
       end
 
+      # Public: Deploy code to Salesforce.
+      #
+      # zip_file - The base64 encoded contents of the zip file.
+      # options  - Hash of DeployOptions.
+      # 
+      # Returns the id of the AsyncResult.
       def deploy(zip_file, options={})
         response = request(:deploy) do |soap|
           soap.body = {
