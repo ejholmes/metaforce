8ce4fb0920e4c584b5ea19b7095cddd064d6be1e
Eric J. Holmes <eric@ejholmes.net>
2012-12-16 22:58:44 -0800

    Move into client class.

--- lib/metaforce.rb
+++ lib/metaforce.rb
@@ -4,7 +4,6 @@ require 'active_support/core_ext'
 
 require 'metaforce/version'
 require 'metaforce/config'
-require 'metaforce/login'
 require 'metaforce/job'
 require 'metaforce/abstract_client'
 require 'metaforce/services/client'
@@ -12,58 +11,14 @@ require 'metaforce/metadata/client'
 
 module Metaforce
   autoload :Manifest, 'metaforce/manifest'
+  autoload :Login,    'metaforce/login'
+  autoload :Client,   'metaforce/client'
 
   class << self
     # Public: Initializes instances of the metadata and services api clients
     # and provides helper methods for deploying and retrieving code.
-    def new(options)
-      Class.new do
-        def initialize(options)
-          @options = options
-        end
-
-        # Public: Used to interact with the Metadata API.
-        def metadata
-          @metadata ||= Metadata::Client.new(@options)
-        end
-
-        # Public: Used to interact with the Services API.
-        def services
-          @services ||= Services::Client.new(@options)
-        end
-
-        # Public: Deploy code to Salesforce.
-        #
-        # path    - A path to a zip file, or a directory to deploy.
-        # options - Deploy options.
-        #
-        # Examples
-        #
-        #   client.deploy(File.expand_path('./src'))
-        def deploy(path, options={})
-          Job::Deploy.new(metadata, path, options)
-        end
-
-        def retrieve(options={})
-          Job::Retrieve.new(metadata, options)
-        end
-
-        # Public: Retrieves files specified in the manifest file (A package.xml
-        # file).
-        def retrieve_unpackaged(manifest, options={})
-          package = if manifest.is_a?(Metaforce::Manifest)
-            manifest
-          elsif manifest.is_a?(String)
-            Metaforce::Manifest.new(File.open(manifest).read)
-          end
-          options = {
-            :api_version    => Metaforce.configuration.api_version,
-            :single_package => true,
-            :unpackaged     => { :types => package.to_package }
-          }.merge(options)
-          retrieve(options)
-        end
-      end.new(options)
+    def new(*args)
+      Client.new(*args)
     end
 
     # Performs a login and retrurns the session
